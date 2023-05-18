7a89e0b66354ab200e7d1db056811921e32113c0
itskleb <itskleb@gmail.com>
2023-05-18 19:26:32 -0400

    Update firebase_test.py

--- firebase_test.py
+++ firebase_test.py
@@ -208,8 +208,7 @@ with tab3:
    #out_df = st.session_state.adj_df
    #out_df_scores = pd.DataFrame(out_df[score_list].sum(axis=1).sort_values(ascending=False))
    see_it = st.session_state.adj_df[score_list].set_index('patrol')
-   see_it
-   see_it['total'] = see_it.sum(axis=1)
+   see_it['total'] = see_it.drop('unit').sum(axis=1)
    see_it = see_it.sort_values(by='total',ascending=False)
    col1, col2, col3 = st.columns(3)
    col1.metric(label=f"{see_it['total'].index[0]} {see_it['unit'][0]}",value=round(see_it['total'][0],3))
