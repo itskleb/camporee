5c67d6eed737f707ef8a7696abb83e30e1011006
itskleb <itskleb@gmail.com>
2023-05-19 15:38:51 -0400

    Update camporee_display.py

--- camporee_display.py
+++ camporee_display.py
@@ -3,4 +3,15 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.set_page_config(title='2023 Aquehonga District Camporee', page_icon="
+st.set_page_config(page_title='Camporee Scoring',page_icon="🏆")
+
+firebaseConfig = {
+ "apiKey": st.secrets['apiKey'],
+ "authDomain": st.secrets['authDomain'],
+ "projectId": st.secrets['projectId'],
+ "storageBucket": st.secrets['storageBucket'],
+ "messagingSenderId": st.secrets['messagingSenderId'],
+  "appId": st.secrets['appId'],
+  "databaseURL": st.secrets['databaseURL']
+}
+stations = ['Check-In','Archery','Fishing','Shelter Building','Semaphore','Fire Building','Two Person Saw','Orienteering','Cooking','First Aid','Animal Prints','Leader Bonus']
