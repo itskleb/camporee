fbb375c978e2f11d56b7aea95c5bec7b88e3efd3
itskleb <itskleb@gmail.com>
2023-05-20 14:33:07 -0400

    Update firebase_test.py

--- firebase_test.py
+++ firebase_test.py
@@ -177,10 +177,11 @@ try:
     st.session_state.adj_df['unit'] = st.session_state.adj_df['index'].apply(lambda x: x.split('***')[1])
     st.session_state.adj_df['patrol'] = st.session_state.adj_df['index'].apply(lambda x: x.split('***')[0])
     scoreseries = st.session_state.adj_df[score_list].groupby(by='unit').sum().sum(axis=1)/st.session_state.adj_df[score_list].groupby(by='unit').count()[temp_list[2]]
-    #scoreseries = pd.DataFrame(scoreseries)
-    #scoreseries['num_patrols']=scoreseries.index.map(st.session_state.adj_df[score_list].groupby(by='unit').count()[temp_list[2]])
-    #scoreseries.columns=['Average Score','Number of Patrols']
-    #scoreseries.sort_values(by='Average Score',ascending=False,inplace=True)
+    scoreseries = pd.DataFrame(scoreseries)
+    scoreseries['num_patrols']=scoreseries.index.map(st.session_state.adj_df[score_list].groupby(by='unit').count()[temp_list[2]])
+    scoreseries.columns=['Average Score','Number of Patrols']
+    scoreseries.sort_values(by='Average Score',ascending=False,inplace=True)
+    scoreseries
 except:
     pass
 with tab2:
